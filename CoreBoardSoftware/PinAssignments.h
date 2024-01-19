1b445f9ae15f2a4cfa65dfd851b542a73ba848bc
dufish42 <duncanfosterkelley@icloud.com>
2024-01-19 17:30:35 -0600

    updated pin assignments

--- CoreBoardSoftware/PinAssignments.h
+++ CoreBoardSoftware/PinAssignments.h
@@ -2,51 +2,47 @@
 #define PinAssignments_h
 
 //Drive Pins
-#define FL_TX           17
-#define FL_RX           16
+#define FL_TX           14
+#define FL_RX           15
 #define ML_TX           29
 #define ML_RX           28
-#define BL_TX           1
-#define BL_RX           0
+#define BL_TX           24
+#define BL_RX           25
 
-#define FR_TX           8
-#define FR_RX           7
-#define MR_TX           35
-#define MR_RX           34
-#define BR_TX           14
-#define BR_RX           15
+#define FR_TX           1
+#define FR_RX           0
+#define MR_TX           20
+#define MR_RX           21
+#define BR_TX           17
+#define BR_RX           16
 
-#define FL_SERIAL       Serial4
+#define FL_SERIAL       Serial3
 #define ML_SERIAL       Serial7
-#define BL_SERIAL       Serial1
-#define FR_SERIAL       Serial2
-#define MR_SERIAL       Serial8
-#define BR_SERIAL       Serial3
+#define BL_SERIAL       Serial6
+#define FR_SERIAL       Serial1
+#define MR_SERIAL       Serial5
+#define BR_SERIAL       Serial4
 
 //Servo Pins
-#define SERVO_1         3
-#define SERVO_2         4
-#define SERVO_3         33
-#define SERVO_4         2
+#define SERVO_1         2
+#define SERVO_2         3
+#define SERVO_3         4
+#define SERVO_4         5
 #define SERVO_5         6
-#define SERVO_6         9
-#define SERVO_7         12
-#define SERVO_8         19
-#define SERVO_9         18
+#define SERVO_6         7
+#define SERVO_7         8
+#define SERVO_8         9
+#define SERVO_9         10
 
 //NeoPixels Pins
 #define NEOPIXEL        11
 
-//LED Pins
-#define LED_TX          22
-#define LED_RX          23
-
 //Button Pins
-#define B_ENC_0         37
-#define B_ENC_1         36
-#define B_ENC_2         25
-#define B_ENC_3         24
+#define B_ENC_0         19
+#define B_ENC_1         18
+#define B_ENC_2         34
+#define B_ENC_3         33
 
-#define REVERSE         5
+#define REVERSE         27
 
 #endif
