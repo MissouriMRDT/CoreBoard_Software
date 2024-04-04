8678382dd61a28e6bec30e043de4e9b06402cb8d
dufish42 <duncanfosterkelley@icloud.com>
2024-04-03 20:18:25 -0500

    updated pin assignments for rev2

--- CoreBoardSoftware/PinAssignments.h
+++ CoreBoardSoftware/PinAssignments.h
@@ -2,19 +2,19 @@
 #define PinAssignments_h
 
 //Drive Pins
-#define FL_TX           14
-#define FL_RX           15
-#define ML_TX           29
-#define ML_RX           28
-#define BL_TX           24
-#define BL_RX           25
+#define FL_TX           29
+#define FL_RX           28
+#define ML_TX           24
+#define ML_RX           25
+#define BL_TX           35
+#define BL_RX           34
 
-#define FR_TX           1
-#define FR_RX           0
-#define MR_TX           20
-#define MR_RX           21
-#define BR_TX           17
-#define BR_RX           16
+#define FR_TX           20
+#define FR_RX           21
+#define MR_TX           17
+#define MR_RX           16
+#define BR_TX           14
+#define BR_RX           15
 
 #define FL_SERIAL       Serial3
 #define ML_SERIAL       Serial7
@@ -24,25 +24,29 @@
 #define BR_SERIAL       Serial4
 
 //Servo Pins
-#define SERVO_1         2
-#define SERVO_2         3
-#define SERVO_3         4
-#define SERVO_4         5
-#define SERVO_5         6
-#define SERVO_6         7
-#define SERVO_7         8
-#define SERVO_8         9
-#define SERVO_9         10
+#define SERVO_1         0
+#define SERVO_2         1
+#define SERVO_3         2
+#define SERVO_4         3
+#define SERVO_5         4
+#define SERVO_6         5
+#define SERVO_7         6
+#define SERVO_8         7
+#define SERVO_9         8
 
 //NeoPixels Pins
 #define NEOPIXEL        11
 
 //Button Pins
-#define B_ENC_0         19
-#define B_ENC_1         18
-#define B_ENC_2         34
+#define B_ENC_0         23
+#define B_ENC_1         21
+#define B_ENC_2         41
 #define B_ENC_3         33
 
 #define REVERSE         27
+
+//Accelerometer Pins
+#define AccSCL          19
+#define ACCSDA          18
 
 #endif
