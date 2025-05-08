aad7d5bfc519b73910e36494a8a8fa8e9093c811
Jesse Deuel <jesse@deuels.net>
2025-05-07 21:28:25 -0500

    Added RPM Control as default drive mode

--- src/CoreBoardSoftware.cpp
+++ src/CoreBoardSoftware.cpp
@@ -21,7 +21,13 @@ void setup() {
     servo1.attach(SERVO_9, 500, 2500);
     servo2.attach(SERVO_8, 500, 2500);
 
-    
+    RoveVESC *motors[6] = { &FL_Motor, &ML_Motor, &BL_Motor, &FR_Motor, &MR_Motor, &BR_Motor };
+    for (RoveVESC *motor : motors) {
+        motor->setPoles(14);
+        motor->setGearRatio(9); // 9 : 1 input/output
+        motor->setMaxRPM(840); // About 30 mph
+    }
+
     //Initialize VESC serial ports
     FL_SERIAL.begin(115200);
     ML_SERIAL.begin(115200);
@@ -235,12 +241,12 @@ void loop() {
 
         // because drive() also does speed ramping, we can't do caching like for the servos
         // convert to decipercent so RoveVESC can convert BACK to a float
-        FL_Motor.drive((int16_t)(motorTargets[0] * 1000));
-        ML_Motor.drive((int16_t)(motorTargets[1] * 1000));
-        BL_Motor.drive((int16_t)(motorTargets[2] * 1000));
-        FR_Motor.drive((int16_t)(motorTargets[3] * 1000));
-        MR_Motor.drive((int16_t)(motorTargets[4] * 1000));
-        BR_Motor.drive((int16_t)(motorTargets[5] * 1000));
+        FL_Motor.driveRPM(motorTargets[0]);
+        ML_Motor.driveRPM(motorTargets[1]);
+        BL_Motor.driveRPM(motorTargets[2]);
+        FR_Motor.driveRPM(motorTargets[3]);
+        MR_Motor.driveRPM(motorTargets[4]);
+        BR_Motor.driveRPM(motorTargets[5]);
 
         leftDriveServo.write();
         leftPanServo.write();
