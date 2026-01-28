b274a414a15913f7953c934dea64611fc9ab7cb1
OcelotEmpire <hobbz.pi@gmail.com>
2026-01-28 17:55:46 -0600

    assign pins

--- include/PinAssignments.h
+++ include/PinAssignments.h
@@ -2,74 +2,67 @@
 #define PIN_ASSIGNMENTS_H
 
 //Drive Pins
-#define FL_TX           29
-#define FL_RX           28
-#define ML_TX           24
-#define ML_RX           25
-#define BL_TX           35
-#define BL_RX           34
+#define FL_TX           35
+#define FL_RX           34
+#define ML_TX           14
+#define ML_RX           15
+#define BL_TX           20
+#define BL_RX           21
 
-#define FR_TX           20
-#define FR_RX           21
-#define MR_TX           17
-#define MR_RX           16
-#define BR_TX           14
-#define BR_RX           15
+#define FR_TX           29
+#define FR_RX           28
+#define MR_TX           24
+#define MR_RX           25
+#define BR_TX           1
+#define BR_RX           0
 
-#define FL_SERIAL       Serial7
-#define ML_SERIAL       Serial6
-#define BL_SERIAL       Serial8
-#define FR_SERIAL       Serial5
-#define MR_SERIAL       Serial4
-#define BR_SERIAL       Serial3
+#define FL_SERIAL       Serial8
+#define ML_SERIAL       Serial3
+#define BL_SERIAL       Serial5
+#define FR_SERIAL       Serial7
+#define MR_SERIAL       Serial6
+#define BR_SERIAL       Serial1
 
 //Servo Pins
-#define SERVO_1         0
-#define SERVO_2         1
-#define SERVO_3         2
-#define SERVO_4         3
-#define SERVO_5         4
-#define SERVO_6         5
-#define SERVO_7         6
-#define SERVO_8         7
-#define SERVO_9         8
+#define LEFT_TILT_SERVO       8
+#define LEFT_PAN_SERVO        7
+#define RIGHT_TILT_SERVO      6
+#define RIGHT_PAN_SERVO       2
+#define BACK_TILT_SERVO       11
+#define BACK_PAN_SERVO        12
+#define SPARE_1_SERVO         22
+#define SPARE_2_SERVO         5
 
 //NeoPixels Pins
-#define NEOPIXEL        11
+#define BACK_STRIP_PIN        17
+#define INNER_STRIP_PIN       16
 
 //Button Pins
-#define B_ENC_0         23
-#define B_ENC_1         22
-#define B_ENC_2         41
-#define B_ENC_3         33
+#define RTRY_1          26
+#define RTRY_2          30
+#define RTRY_4          31
 
-#define REVERSE         10
+#define DIR_FORWARD     32
+#define DIR_BACK        13
+#define DIR_RIGHT       23
+#define DIR_LEFT        27
 
-//Switch Pins
-#define FL_SWITCH       38
-#define ML_SWITCH       27
-#define BL_SWITCH       26
-#define FR_SWITCH       12
+//Motor Switches
+#define FL_SWITCH       36
+#define ML_SWITCH       37
+#define BL_SWITCH       38
+#define FR_SWITCH       39
 #define MR_SWITCH       40
-#define BR_SWITCH       39
+#define BR_SWITCH       41
 
-//Accelerometer Pins
-#define ACC_SDA         18
-#define ACC_SCL         19
+//Sensor I2C Pins
+#define SENS_SDA         18
+#define SENS_SCL         19
 
-// Button Assignments (Switch Number -> Encoder Number)
-// Motors
-#define DS_EN_BUTTON    8
-
-// Servos
-#define LT_BUTTON       3
-#define LP_BUTTON       2
-#define LD_BUTTON       1
-#define RT_BUTTON       7
-#define RP_BUTTON       10
-#define RD_BUTTON       9
-#define BD_BUTTON       6
-#define S1_BUTTON       4
-#define S2_BUTTON       5
+//Fan Pins
+#define FAN_PWM_1        10
+#define FAN_PWM_2        9
+#define FAN_TACH_1       3
+#define FAN_TACH_2       4
 
 #endif
